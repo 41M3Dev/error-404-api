c6201675f15d48722e34ac1628fb1e300ba5b5f1
Donat Dorian <donat.dorian@nfs.school>
2026-05-29 16:18:28 +0200

    migration promos terminer

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
+    client: 'mysql2',
     development: {
-        client: 'mysql2',
         connection: {
             host: process.env.DB_HOST,
             port: process.env.DB_PORT,
@@ -18,20 +18,4 @@ module.exports = {
         },
     },
 
-    production: {
-        client: 'mysql2',
-        connection: {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-        },
-        migrations: {
-            directory: './migrations',
-        },
-        seeds: {
-            directory: './seeds',
-        },
-    },
 };
