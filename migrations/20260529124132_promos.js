ac2cc7db47e343bb0bb2fa351d41c25d5a47bcd7
Aimé Adjassa <aimeadjassa@Aimes-MacBook-Air.local>
2026-05-29 14:42:14 +0200

    Création de la migration de promo

---
+++ migrations/20260529124132_promos.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("promos", (table) => {
+        table.increments("id",50).primary();
+        table.string("name").notNullable();
+        table.tinyint("is_active").notNullable().defaultTo(1);
+        table.date("created_at").defaultTo(knex.fn.now());
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("promos");
+};
