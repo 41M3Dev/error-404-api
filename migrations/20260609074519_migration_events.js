cd6f5b6a7720cadd096b95e186b73ff8dae49997
Donat Dorian <donat.dorian@nfs.school>
2026-06-10 16:00:34 +0200

    features/migration Ajout de la table events

--- migrations/20260609074519_migration_events.js
+++ migrations/20260609074519_migration_events.js
@@ -2,8 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    
+exports.up = function(knex){
+    return knex.schema.createTable("events", (table) => {
+        table.increments("id").primary();
+        table.string("title", 150).notNullable();
+        table.string("slug", 180).notNullable().unique();
+        table.text("description");
+        table.string("place", 255).notNullable();
+        table.decimal("price", 10, 2).notNullable().defaultTo(0.00);
+        table.integer("max_participants");
+        table.dateTime("start_datetime").notNullable();
+        table.dateTime("end_datetime").notNullable();
+        table.dateTime("registration_deadline");
+        table.text("image_url");
+        table.enum("status", ["draft", "published", "cancelled", "full"]).notNullable().defaultTo("draft");
+        table.integer("created_by").unsigned().notNullable();
+        table.foreign("created_by").references("id").inTable("users");
+        table.timestamps(true, true);
+    });
 };
 
 /**
@@ -11,5 +27,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-    
+    return knex.schema.dropTableIfExists("events");
 };
