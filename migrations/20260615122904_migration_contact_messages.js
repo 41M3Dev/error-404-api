a9f944eedb07706e7e45f82effd4ad29d0dea853
Donat Dorian <donat.dorian@nfs.school>
2026-06-15 14:55:19 +0200

    features/migration Ajout de la table contact_messages

---
+++ migrations/20260615122904_migration_contact_messages.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("contact_messages", (table) => {
+        table.increments("id").primary();
+        table.string("full_name", 150).notNullable();
+        table.string("email", 150).notNullable();
+        table.string("subject", 150);
+        table.text("message").notNullable();
+        table.enum("status", ["nouveau", "lu", "repondu"]).notNullable().defaultTo("nouveau");
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("contact_messages");
+};
