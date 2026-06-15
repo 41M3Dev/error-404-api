eaa2bd97e9df64e70f963c73b8876f313d4dc0c0
Donat Dorian <donat.dorian@nfs.school>
2026-06-15 14:12:50 +0200

    features/migration Ajout de la table gallery_images

---
+++ migrations/20260615115633_migration_galleries_image.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("gallery_images", (table) => {
+        table.increments("id").primary();
+        table.integer("gallery_id").unsigned().notNullable();
+        table.foreign("gallery_id").references("id").inTable("galleries");
+        table.text("image_url").notNullable();
+        table.string("alt_text", 255);
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("gallery_images");
+};
