cab4d72d67817fe79e77da32ad66fb63be9fde1a
Donat Dorian <donat.dorian@nfs.school>
2026-06-17 16:46:22 +0200

    features/init-routes-controller Ajout des dossiers Routes,Controllers,ajout des fichiers Promos route et controller puis modification de app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const promosRoutes = require("./src/routes/promos.routes");
+app.use("/api/promos", promosRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`L'API est lancée sur le port : ${PORT}`);
