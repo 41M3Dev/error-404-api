bd12a875c31d5b587019eb602a66048e02552171
DDo <donat.dorian@nfs.school>
2026-06-24 17:16:03 +0200

    routes/controllers Ajout du Middleware.

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({message: "Accès refusé. Aucun badge fourni."});
+        }
+
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+        next();
+
+    } catch(error) {
+        return res.status(403).json({message: "Badge invalide ou expiré. Veuillez vous reconnecter."});
+    }
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/register", usersController.register);
 router.post("/login", usersController.login);
-router.get("/", usersController.getAllUsers);
+router.get("/", authMiddleware.verifyToken, usersController.getAllUsers);
 router.get("/:id", usersController.getUserById);
 
 module.exports = router
